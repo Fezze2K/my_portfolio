250a5273d1796b54b88cde388371f320e273a3c6
Faizan Mian <willskill@macbook-pro-3.lan>
2023-01-24 20:09:15 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Heading from './Heading.jsx';
+
+function App() {
+  return (
+  <div class="flex flex-col h-screen justify-center items-center bg-gradient-to-r from-violet-500 to-fuchsia-500">
+      <Heading>
+      </Heading>
+  </div>
+  );
+}
+
+export default App;
---
+++ src/Heading.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function Heading() {
+  return (
+
+  <div className='tracking-wider	font-mono text-white text-9xl text-center'>
+      Faizan Mian
+  </div>
+  );
+}
+
+export default Heading;
